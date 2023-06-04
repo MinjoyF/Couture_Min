4429b37f404eae873e07fecc1e58eebbe0c30af1
MinjoyF <ghislainngabiapsi@gmail.com>
2023-06-04 21:39:55 +0200

    implementation of BestNewAccount and some changes

--- src/main/java/org/minjoy/App.java
+++ src/main/java/org/minjoy/App.java
@@ -2,7 +2,9 @@ package org.minjoy;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.minjoy.Model.AccountStatus;
 import org.minjoy.Model.BestAccount;
+import org.minjoy.Model.BestNewAccount;
 import org.minjoy.Model.ClientAccount;
 
 import java.util.*;
@@ -25,21 +27,27 @@ public class App {
         }
 
         Map<String, ClientAccount> clientAccountMap = new HashMap<>();
-        clientAccountMap.put("acc1",new BestAccount("Ghislain", "012733727466",3.5));
+        clientAccountMap.put("acc1",new BestNewAccount("Ghislain", "012733727466",3.5));
         clientAccountMap.put("acc2",new BestAccount("Nicaise", "012733727466",3.5));
-        clientAccountMap.put("acc3",new BestAccount("Ornella", "012733727466",3.5));
+        clientAccountMap.put("acc3",new BestNewAccount("Ornella", "012733727466",3.5));
         clientAccountMap.put("acc4",new BestAccount("Prudence", "012733727466",3.5));
         clientAccountMap.put("acc5",new BestAccount("Megane", "012733727466",3.5));
+        clientAccountMap.put("acc6",new BestNewAccount("Pascaline", "012733727466",3.5));
+
         System.out.println("-------CLIENT1---------");
         ClientAccount acc= clientAccountMap.get("acc2");
         System.out.println(acc.geType());
 
         System.out.println("-------CLIENT2---------");
+        ClientAccount acc1= clientAccountMap.get("acc6");
+        System.out.println(toJson(acc1));
+
+        System.out.println("-------CLIENT3---------");
         for(String key: clientAccountMap.keySet()){
             System.out.println(toJson(clientAccountMap.get(key)));
         }
 
-        System.out.println("-------CLIENT3---------");
+        System.out.println("-------CLIENT4---------");
         for( ClientAccount ca: clientAccountMap.values()){
             System.out.println(toJson(ca));
         }
